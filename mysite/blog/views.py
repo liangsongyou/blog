5a8e69fa0515d411220cdc41ee93bc1762e3d36c
yuebei <yuebei58@gmail.com>
2018-06-29 17:08:31 +0800

    add static templates and pagination

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,12 +1,23 @@
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger 
 from django.shortcuts import render, get_object_or_404
+
 from .models import Post
 
 
 def post_list(request):
-    posts = Post.published.all()
+    object_list = Post.published.all()
+    paginator   = Paginator(object_list, 3)
+    page        = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
     return render(request, 
                 'blog/post/list.html',
-                {'posts':posts})
+                {'page':page,
+                'posts':posts})
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post,
@@ -17,4 +28,3 @@ def post_detail(request, year, month, day, post):
     return render(request,
                   'blog/post/detail.html',
                   {'post':post})
-    
