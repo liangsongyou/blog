dd4435d6ada26ec402a6aa55bce4fffa8500a703
yuebei <yuebei58@gmail.com>
2018-06-30 19:04:53 +0800

    add tag to post

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -3,9 +3,34 @@ from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 from django.core.mail import send_mail
 
+from taggit.models import Tag
+
 from .models import Post, Comment
 from .forms import EmailPostForm, CommentForm
 
+
+def post_list(request, tag_slug=None):
+    object_list = Post.published.all()
+    tag = None
+
+    if tag_slug:
+        tag = get_object_or_404(Tag,slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+
+    paginator = Paginator(object_list, 3)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+
+    return render(request,
+                  'blog/post/list.html',
+                  {'page':page,
+                   'posts':posts,
+                   'tag':tag})
 
 class PostListView(ListView):
     queryset = Post.published.all()
