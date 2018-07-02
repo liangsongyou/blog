32877a11d6e0dd53218f54206754b920976182c8
yuebei <yuebei58@gmail.com>
2018-07-02 17:05:37 +0800

    add search to blog

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,9 +1,13 @@
-from django.contrib.postgres.search import SearchVector
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger 
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 from django.core.mail import send_mail
 from django.db.models import Count
+from django.contrib.postgres.search import (
+                                            SearchVector, 
+                                            SearchQuery, 
+                                            SearchRank, 
+                                            TrigramSimilarity)
 
 from taggit.models import Tag
 
@@ -106,9 +110,15 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
+            search_vector = SearchVector('title',weight='A') + SearchVector('body', weight='B')
+            search_query  = SearchQuery(query)
             results = Post.objects.annotate(
-                search=SearchVector('title','body'),
-            ).filter(search=query)
+                search=search_vector,
+                rank=SearchRank(search_vector,search_query),
+            ).filter(rank__gte=0.3).order_by('-rank')
+            # results = Post.objects.annotate(
+            #     similarity=TrigramSimilarity('title',query),
+            # ).filter(similarity__gt=0.3).order_by('-similarity')
     return render(request,
                   'blog/post/search.html',
                   {'form':form,
